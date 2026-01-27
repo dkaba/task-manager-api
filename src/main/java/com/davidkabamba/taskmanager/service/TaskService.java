c77b0cfb5ba41f4c81ece47995866361faddb38c
dkaba <david.kabamba@gmail.com>
2026-01-27 17:20:06 +0100

    Modifies message for task not found

--- src/main/java/com/davidkabamba/taskmanager/service/TaskService.java
+++ src/main/java/com/davidkabamba/taskmanager/service/TaskService.java
@@ -21,7 +21,7 @@ public class TaskService {
 
     public Task findById(Long id) {
         return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Task not found"));
+                .orElseThrow(() -> new RuntimeException("Task was not found"));
     }
 
     public Task create(Task task) {
