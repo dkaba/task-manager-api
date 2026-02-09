f88dff9460c6d837a0d463ea37bb44c3b6de001c
dkaba <david.kabamba@gmail.com>
2026-02-09 21:42:39 +0100

    Add auth controller

--- src/main/java/com/davidkabamba/taskmanager/controller/AuthController.java
+++ src/main/java/com/davidkabamba/taskmanager/controller/AuthController.java
@@ -1,4 +1,42 @@
 package com.davidkabamba.taskmanager.controller;
 
+import com.davidkabamba.taskmanager.model.AuthRequest;
+import com.davidkabamba.taskmanager.model.AuthResponse;
+import com.davidkabamba.taskmanager.security.JwtService;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUsername(),
+                            request.getPassword()
+                    )
+            );
+
+            String token = jwtService.generateToken(request.getUsername());
+            return new AuthResponse(token);
+
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("Invalid username or password");
+        }
+    }
 }
--- src/main/java/com/davidkabamba/taskmanager/model/AuthResponse.java
+++ src/main/java/com/davidkabamba/taskmanager/model/AuthResponse.java
@@ -1,4 +1,20 @@
 package com.davidkabamba.taskmanager.model;
 
 public class AuthResponse {
+
+    private String token;
+
+    public AuthResponse() {}
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
